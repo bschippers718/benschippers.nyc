8a1931186a66e3e1168cac883b8f397f79c34932
bschippers718 <bschippers718@gmail.com>
2015-06-08 16:48:42 -0400

    changing routes
    get to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/me'
+  root "pages#me"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
