d666187385da600c2b7d2336281fa691ca16f073
bschippers718 <bschippers718@gmail.com>
2015-07-20 14:30:27 -0400

    new background
    Coney Island

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def me
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  
+  root "pages#about"
+  get 'pages/me'
+
   get 'pages/about'
 
-  root "pages#me"
+  #get 'pages/me'
+
+  #get 'pages/about'
+
+  #root "pages#me"
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
